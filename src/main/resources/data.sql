52de4eaf3addd98a8e9d425170298d48b87e4a7f
rhiodam <rhioda.m@gmail.com>
2019-03-04 20:46:37 +0700

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(1, 'mkyong','111@yahoo.com', TO_DATE('2017-02-11', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(2, 'yflow','222@yahoo.com', TO_DATE('2017-02-12', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(3, 'zilap','333@yahoo.com', TO_DATE('2017-02-13', 'yyyy-mm-dd'));
+
+
+
+
+insert into Team (id,name) values(1,'Barcelona');
+insert into Player (id, team_id, name, num, position) values(1,1,'Lionel Messi', 10, 'Forward');
+insert into Player (id, team_id, name, num, position) values(2,1,'Andreas Inniesta', 8, 'Midfielder');
+insert into Player (id, team_id, name, num, position) values(3,1,'Pique', 3, 'Defender');
+
